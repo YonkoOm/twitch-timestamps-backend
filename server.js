1c2d43375c689f53b9af215bbe0468db7d58aa2f
Om Moradia <moradiaom1@gmail.com>
2025-03-06 15:47:42 -0600

    implemented functionality to fetch an access token, renamed some endpoints, and better error checking

--- server.js
+++ server.js
@@ -2,75 +2,172 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config();
+if (process.env.NODE_ENV !== "production") {
+  dotenv.config();
+}
 
 const app = express();
 app.use(cors());
 
-const CLIENT_ID = process.env.CLIENT_ID;
-const ACCESS_TOKEN = process.env.ACCESS_TOKEN;
+const PORT = 3000;
 
-app.get("/user", async (req, res) => {
+const CLIENT_ID = process.env.CLIENT_ID;
+const CLIENT_SECRET = process.env.CLIENT_SECRET;
+
+let accessToken;
+let tokenExpiration;
+
+// consider the token as expired if less than an hour is left until the token expires
+const isTokenExpired = () => tokenExpiration - Date.now() < 1000 * 60 * 60;
+
+const fetchAccessToken = async () => {
+  if (accessToken && tokenExpiration && !isTokenExpired()) {
+    return accessToken;
+  }
+
+  const url = "https://id.twitch.tv/oauth2/token";
+  const body = new URLSearchParams({
+    client_id: CLIENT_ID,
+    client_secret: CLIENT_SECRET,
+    grant_type: "client_credentials",
+  });
+
+  try {
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: body,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to fetch Twitch OAuth token: ${response.status}`);
+    }
+
+    const res = await response.json();
+    accessToken = res.access_token;
+    tokenExpiration = Date.now() + res.expires_in * 1000;
+    return accessToken;
+  } catch (e) {
+    console.error("Error fetching Twitch OAuth token:", e);
+    return null;
+  }
+};
+
+const attachToken = async (req, res, next) => {
+  const token = await fetchAccessToken();
+  if (!token) {
+    res.status(500).json({ error: "Failed to get access token" });
+  }
+  req.accessToken = token;
+  next();
+};
+
+// return data if the streamer is live, if they are not, returns an empty array
+app.get("/streamer", attachToken, async (req, res) => {
   const streamer = req.query.login;
   const url = `https://api.twitch.tv/helix/users?login=${streamer}`;
 
-  const response = await fetch(url, {
-    headers: {
-      "Client-ID": CLIENT_ID,
-      Authorization: `Bearer ${ACCESS_TOKEN}`,
-    },
-  });
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Client-ID": CLIENT_ID,
+        Authorization: `Bearer ${req.accessToken}`,
+      },
+    });
 
-  const data = await response.json();
-  res.json(data);
+    if (!response.ok) {
+      throw new Error("Error fetching info on streamer's livestream");
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    console.error(e);
+    res
+      .status(500)
+      .json({ message: "Failed to get information on the streamer" });
+  }
 });
 
-app.get("/video", async (req, res) => {
+app.get("/vod", attachToken, async (req, res) => {
   const videoId = req.query.id;
-  const url = `https://api.twitch.tv/helix/videos?id=${videoId}`;
 
-  const response = await fetch(url, {
-    headers: {
-      "Client-ID": CLIENT_ID,
-      Authorization: `Bearer ${ACCESS_TOKEN}`,
-    },
-  });
+  try {
+    const url = `https://api.twitch.tv/helix/videos?id=${videoId}`;
 
-  const data = await response.json();
-  res.json(data);
+    const response = await fetch(url, {
+      headers: {
+        "Client-ID": CLIENT_ID,
+        Authorization: `Bearer ${req.accessToken}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Error fetching VOD data");
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Failed to complete VOD request" });
+  }
 });
 
-app.get("/livestream", async (req, res) => {
+app.get("/livestream", attachToken, async (req, res) => {
   const username = req.query.username;
-  const response = await fetch(
-    `https://api.twitch.tv/helix/streams?user_login=${username}`,
-    {
-      headers: {
-        "Client-ID": CLIENT_ID,
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
+
+  try {
+    const response = await fetch(
+      `https://api.twitch.tv/helix/streams?user_login=${username}`,
+      {
+        headers: {
+          "Client-ID": CLIENT_ID,
+          Authorization: `Bearer ${req.accessToken}`,
+        },
       },
-    },
-  );
+    );
 
-  const data = await response.json();
+    if (!response.ok) {
+      throw new Error("Error fetching livestream data");
+    }
 
-  return res.json(data);
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ message: "Failed to complete livestream request" });
+  }
 });
 
-app.get("/vods/:streamerId", async (req, res) => {
+app.get("/vods/:streamerId", attachToken, async (req, res) => {
   const streamerId = req.params.streamerId;
-  const response = await fetch(
-    `https://api.twitch.tv/helix/videos?user_id=${streamerId}&type=archive`,
-    {
-      headers: {
-        "Client-ID": CLIENT_ID,
-        Authorization: `Bearer ${ACCESS_TOKEN}`,
-      },
-    },
-  );
-  const data = await response.json();
 
-  res.json(data);
+  try {
+    const response = await fetch(
+      `https://api.twitch.tv/helix/videos?user_id=${streamerId}&type=archive`,
+      {
+        headers: {
+          "Client-ID": CLIENT_ID,
+          Authorization: `Bearer ${req.accessToken}`,
+        },
+      },
+    );
+
+    if (!response.ok) {
+      throw new Error("Error fetching the streamer's VODs");
+    }
+
+    const data = await response.json();
+    res.json(data);
+  } catch (e) {
+    console.error(e);
+    res
+      .status(500)
+      .json({ message: "Failed to complete request for streamer's VODs" });
+  }
 });
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
